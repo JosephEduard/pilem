46ca8a8d59792b5f4956622b4116d2ecfcc7e9da
JosephEduard <josepheduardulyloni_2327250080@mhs.mdp.ac.id>
2025-02-26 09:21:46 +0700

    second commit

---
+++ pilem/lib/screens/home_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:pilem/models/movie.dart';
+import 'package:pilem/services/api_service.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final ApiServices _apiServices = ApiServices();
+  List<Movie> _allMovies = [];
+  List<Movie> _trendingMovies = [];
+  List<Movie> _popularMovies = [];
+
+  Future<void> _loadMovies() async {
+    final List<Map<String, dynamic>> allMoviesData =
+        await _apiServices.getAllMovies();
+
+    setState(() {
+      _allMovies = allMoviesData.map((e) => Movie.fromJson(e)).toList();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMovies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pilem')),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "All Movies",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: _allMovies.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final Movie movie = _allMovies[index];
+                  return Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Image.network(
+                          'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                          width: 120,
+                          height: 160,
+                          fit: BoxFit.cover,
+                        ),
+                        Text(movie.title),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
